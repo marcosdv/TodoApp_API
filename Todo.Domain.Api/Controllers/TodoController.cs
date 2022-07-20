85d9c82792d07e8cc795660f21dd67a67f4ae130
marcosdv <marcosdiasvendramini@yahoo.com.br>
2022-07-20 00:32:39 -0300

    Curso finalizado

---
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Todo.Domain.Commands;
+using Todo.Domain.Entities;
+using Todo.Domain.Handlers;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/todos")]
+    [Authorize]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll([FromServices] ITodoRepository repository)
+        {
+            var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetAll(user);
+        }
+
+        [Route("done")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDone([FromServices] ITodoRepository repository)
+        {
+            var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetAllDone(user);
+        }
+
+        [HttpGet("undone")]
+        public IEnumerable<TodoItem> GetUndone([FromServices] ITodoRepository repository)
+        {
+            var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetAllUndone(user);
+        }
+
+        [HttpGet("done/today")]
+        public IEnumerable<TodoItem> GetDoneToday([FromServices] ITodoRepository repository)
+        {
+            var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetByPeriod(user, DateTime.Now.Date, true);
+        }
+
+        [HttpGet("undone/today")]
+        public IEnumerable<TodoItem> GetUndoneToday([FromServices] ITodoRepository repository)
+        {
+            var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetByPeriod(user, DateTime.Now.Date, false);
+        }
+
+        [HttpGet("done/tomorrow")]
+        public IEnumerable<TodoItem> GetDoneTomorrow([FromServices] ITodoRepository repository)
+        {
+            var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetByPeriod(user, DateTime.Now.Date.AddDays(1), true);
+        }
+
+        [HttpGet("undone/tomorrow")]
+        public IEnumerable<TodoItem> GetUndoneTomorrow([FromServices] ITodoRepository repository)
+        {
+            var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetByPeriod(user, DateTime.Now.Date.AddDays(1), false);
+        }
+
+        [HttpPost]
+        public GenericCommandResult Create([FromBody] CreateTodoCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [HttpPut]
+        public GenericCommandResult Update([FromBody] UpdateTodoCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [HttpPut("mark-as-done")]
+        public GenericCommandResult MarkAsDone([FromBody] MarkTodoAsDoneCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [HttpPut("mark-as-undone")]
+        public GenericCommandResult MarkAsUndone([FromBody] MarkTodoAsUndoneCommand command, [FromServices] TodoHandler handler)
+        {
+            command.User = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return (GenericCommandResult)handler.Handle(command);
+        }
+    }
+}
--- Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
+++ Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
@@ -13,4 +13,24 @@ internal class FakeTodoRepository : ITodoRepository
     {
         return new TodoItem("Titulo ToDo", DateTime.Now, "NomeUsuario");
     }
+
+    public IEnumerable<TodoItem> GetAll(string user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TodoItem> GetAllDone(string user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TodoItem> GetAllUndone(string user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TodoItem> GetByPeriod(string user, DateTime date, bool done)
+    {
+        throw new NotImplementedException();
+    }
 }
